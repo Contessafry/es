7833ccf2068ac5143302e4bf1ec69856788014e6
Francesco Marino <francescofriy@gmail.com>
2023-12-05 15:05:21 +0100

    feat(html)/add counter

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-let pallino = document.getElementById("pallino");
-pallino.addEventListener("click", function () {
-  console.log("bottone premuto");
+let sx = document.getElementById("sx");
+let dx = document.getElementById("dx");
+let contatore = document.getElementById("contatore");
+let valoreContatore = 0;
+
+dx.addEventListener("click", function () {
+  valoreContatore++;
+  contatore.textContent = valoreContatore;
+});
+
+sx.addEventListener("click", function () {
+  if (valoreContatore > 0) {
+    valoreContatore--;
+    contatore.textContent = valoreContatore;
+  } else {
+    alert("il contatore non può andare sotto zero");
+  }
 });
